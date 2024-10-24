8cf9ea1cfc54394ee1252da81fba0baefbe87227
domi-s <44111026+domi-s@users.noreply.github.com>
2024-10-24 12:06:14 +0100

    Initial commit - Concept

---
+++ src/routes/+page.svelte
@@ -0,0 +1,73 @@
+<div class="absolute z-[1000] top-14 left-14 right-14 text-white flex gap-2">
+    <nav class="ml-auto flex gap-6 items-center text-yellow-100 uppercase text-sm tracking-widest">
+        <a href="#">Home</a>
+        <a href="#">Tickets</a>
+        <a href="#">Important Information</a>
+        <a href="#">Log in</a>
+    </nav>
+</div>
+<div class="relative w-full h-[calc(100vh)]">
+    <video class="absolute w-full h-full object-cover" autoplay loop muted playsinline>
+        <source src="https://www.some.ox.ac.uk/wp-content/uploads/2023/12/SC-Website-Loop-V2_CLIPPED.mp4" type="video/mp4">
+    </video>
+    <div class="absolute inset-0 bg-black bg-opacity-50"></div>
+    <div class="absolute inset-8 border-2 border-solid border-yellow-100"></div>
+    <!-- <div class="absolute inset-0 flex items-center justify-center">
+        <div class="text-white text-center flex flex-col" style="text-shadow: 0 0 1em black;">
+            <h1 class="font-serif text-6xl text-yellow-100">St Catz Ball</h1>
+            <p class="text-lg text-yellow-50">University of Oxford</p>
+        </div>
+    </div> -->
+    <div class="absolute inset-14 flex items-end justify-start">
+        <div class="text-white flex flex-col" style="text-shadow: 0 0 1em black;">
+            <h1 class="font-serif text-8xl text-yellow-100">St Catz Ball</h1>
+            <p class="text-yellow-50 uppercase tracking-widest flex gap-6">
+                <span>University of Oxford</span>
+                <span>2025</span>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="py-16 px-4 bg-slate-950">
+    <div class="max-w-screen-xl mx-auto">
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-4 p-4">
+            <div class="md:col-span-2 bg-slate-900 text-white p-6 flex flex-col gap-4">
+                <h2 class="font-serif text-3xl text-yellow-100">Everything you need to know</h2>
+                <p>
+                    The St. Catherine's College Ball 2025 is set to be an unforgettable evening of elegance, celebration, and indulgence. Nestled within the historic grounds of the University of Oxford, the ball promises an immersive experience, combining tradition with contemporary flair. Guests will be treated to an enchanting night filled with live entertainment, gourmet dining, and captivating performances, all within a stunningly transformed venue.
+                </p>
+                <p>
+                    This year's theme centers on the fusion of timeless sophistication and modern creativity, inviting attendees to explore a world where classic Oxford charm meets cutting-edge design. With meticulously curated experiences at every turn, from music and dancing to artistic installations, the 2025 Ball will be a night of endless discovery. Whether you're looking for a magical escape or a lively night of fun, this year's event will deliver an extraordinary experience that you won't want to miss.
+                </p>
+            </div>
+        
+            <div class="md:col-span-1">
+                <img class="w-full h-full object-cover" src="https://images.pexels.com/photos/1164985/pexels-photo-1164985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Dummy Image 1">
+            </div>
+        
+            <div class="md:col-span-1">
+                <img class="w-full h-full object-cover" src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Dummy Image 2">
+            </div>
+
+            <div class="md:col-span-4 min-h-80 flex items-center justify-center relative">
+                <img class="absolute w-full h-full object-cover opacity-25" src="https://images.pexels.com/photos/13344262/pexels-photo-13344262.jpeg?auto=compress&cs=tinysrgb&w=1920" alt="Theme Background">
+                <div class="relative flex flex-col items-center text-white">
+                    <p class="tracking-widest text-sm opacity-75 mb-2 text-yellow-50">The theme is&hellip;</p>
+                    <h2 class="font-serif text-5xl text-yellow-100">Midnight Reverie</h2>
+                </div>
+            </div>
+
+            <iframe class="md:col-span-2 w-full h-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4694.934127535434!2d-1.2470536038012479!3d51.75746774754416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876c154a2c2921d%3A0x74d94012acb4a403!2sSt%20Catherine&#39;s%20College!5e1!3m2!1sen!2suk!4v1729712200046!5m2!1sen!2suk" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            
+            <div class="md:col-span-2 bg-slate-900 text-white p-6 flex flex-col gap-4">
+                <h2 class="font-serif text-3xl text-yellow-100">Getting there</h2>
+                <p>
+                    St. Catherine’s College Ball 2025 is set in the heart of Oxford, making it easily accessible from various locations. If you're coming from within the city, walking or cycling are great options, allowing you to soak in the picturesque surroundings before arriving. For those traveling from farther away, the college is just a short distance from the central train station and major bus stops, with taxis readily available for convenience.
+                </p>
+                <p>
+                    If you're driving, there are several nearby parking options, but spaces can be limited, so we recommend arriving early. Alternatively, you can take advantage of the Park & Ride services located around Oxford, which offer a stress-free way to get to the event. Keep in mind that Oxford’s city center has some pedestrian-only zones, so plan your route accordingly. No matter how you choose to travel, we’ll ensure your arrival is smooth, so you can enjoy an unforgettable night from the moment you step in.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif'],
+      'serif': ['Old Standard TT', 'serif'],
+    },
+    extend: {}
+  },
+
+  plugins: []
+};
